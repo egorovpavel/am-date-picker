41c152a334462011f9646f5fbb892e8d1e943387
Pavel Egorov <pavel.egorov@salesengineonline.com>
2016-11-15 02:22:29 +0000

    fixed missed constant declaration

--- src/js/am-date-picker.constant.js
+++ src/js/am-date-picker.constant.js
@@ -2,9 +2,10 @@
     'use strict';
 
     var options = ['allowClear', 'backButtonText', 'cancelButton', 'inputLabel',
-                   'maxDate', 'minDate', 'maxYear', 'minYear', 'locale',
-                   'inputDateFormat', 'popupDateFormat', 'showInputIcon', 'todayButton',
-                   'calendarIcon', 'prevIcon', 'nextIcon', 'clearIcon'];
+        'maxDate', 'minDate', 'maxYear', 'minYear', 'locale',
+        'inputDateFormat', 'popupDateFormat', 'showInputIcon', 'todayButton',
+        'calendarIcon', 'prevIcon', 'nextIcon', 'clearIcon', 'fontIcon'
+    ];
 
     angular
         .module('am.date-picker')
